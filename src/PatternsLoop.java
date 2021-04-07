245be5e2e354ddfbaa6caa184191b2630c6190ae
Divya2411 <pdivyapatel@outlook.com>
2021-04-07 11:39:14 -0400

    07 april 2021

--- src/PatternsLoop.java
+++ src/PatternsLoop.java
@@ -3,12 +3,31 @@ public class PatternsLoop {
 
         for(int i = 1; i<= 4; i++){
             for(int j = 1; j<= 4; j++){
+                   if(i==1 | i ==4 | j== 1 | j==4){
+                       System.out.print(" * ");
+                   }else{
+                       System.out.print("   ");
+                   }
 
-                    System.out.println("*");
 
             }System.out.println();
         }
+        int x;
+        int y;
+        for(x = 1; x<= 4; x++){
+            for(y = 1; y<= x; y++){
+                  if((x+y)%2 == 0){
 
+                      System.out.print("1");
+                  }else{
+                      System.out.print("0");
+                  }
+
+
+
+
+            }System.out.println();
+        }
 
 
 
